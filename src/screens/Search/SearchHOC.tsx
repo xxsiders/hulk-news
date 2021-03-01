b8135ea14e531aab2dbc5005f5b31b78084ca767
xxsiders <loayalaa25@gmail.com>
2021-03-01 12:39:34 +0200

    feat: :sparkles: add search news HOC

---
+++ src/screens/Search/SearchHOC.tsx
@@ -0,0 +1,30 @@
+
+import { useRoute } from '@react-navigation/native'
+import React, { useEffect, useState } from 'react'
+import SearchProvider from '../../providers/SearchProvider'
+
+const SearchHOC = (Com: React.ComponentType<any>) => {
+
+    const wrapper = (props: any) => {
+        const [query, setquery] = useState('')
+        const { fetchNews, data, loading } = SearchProvider()
+        const { params }: any = useRoute()
+        useEffect(() => {
+            if (params.query) {
+                setquery(params.query)
+                fetchNews(params.query)
+            }
+        }, [params])
+        return <Com
+            {...props}
+            data={data}
+            loading={loading}
+            fetchNews={fetchNews}
+            setquery={setquery}
+            query={query}
+        />
+    }
+    return wrapper
+}
+
+export default SearchHOC
---
+++ src/screens/Search/index.tsx
@@ -0,0 +1,4 @@
+import Search from "./Search";
+import SearchHOC from "./SearchHOC";
+
+export default SearchHOC(Search)
