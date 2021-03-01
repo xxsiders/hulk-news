f8c916816d11afa3fda08e20c2a31253584f1250
xxsiders <loayalaa25@gmail.com>
2021-03-01 12:21:36 +0200

    feat: :art: add article data types

---
+++ src/constants/Article.tsx
@@ -0,0 +1,14 @@
+export type Article = {
+    source: {
+        id: number,
+        name: string
+    },
+    author: string,
+    title: string,
+    description: string,
+    url: string,
+    urlToImage: string,
+    publishedAt: string,
+    content: string
+}
+
