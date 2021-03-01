e9539c39fa59c94db0b7f63eec537a16feaabc85
xxsiders <loayalaa25@gmail.com>
2021-03-01 12:43:54 +0200

    feat: :sparkles: add app content

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import Loader from './src/components/Loader';
+import AppStack from './src/navigations/AppStack';
+import AppDeafults from './src/roots/AppDeafults';
+import { BootstrapProvider } from './src/roots/Bootstrap';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const { loadConfigs, loaded } = AppDeafults()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    loadConfigs()
+  }, [])
+
+  if (!loaded) return <Loader />
+  else {
+    return <BootstrapProvider>
+      <AppStack />
+    </BootstrapProvider>
+  }
+}
