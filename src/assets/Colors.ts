a3aab594488b07cb4e7135b0b4652db21130381a
xxsiders <loayalaa25@gmail.com>
2021-03-01 12:16:47 +0200

    feat: :lipstick: add assets files

---
+++ src/assets/Colors.ts
@@ -0,0 +1,22 @@
+let DefaultColors = {
+    primary: "#5C4DB1",
+    background: "#FFFFFF",
+    border: "transparent",
+    card: "#e5e5e570",
+    text: "#101010",
+    notification: "#5C4DB1"
+}
+
+let DarkColors = {
+    primary: "#5C4DB1",
+    background: "#000",
+    border: "#dddddd20",
+    card: "#14213d",
+    text: "#aaaaaa",
+    notification: "#5C4DB1"
+}
+
+export default {
+    DefaultColors,
+    DarkColors
+}
---
+++ src/assets/Fonts.js
@@ -0,0 +1,10 @@
+const size = {
+    h1: 36,
+    h2: 30,
+    h3: 26,
+    h4: 21,
+    h5: 16,
+    h6: 12
+}
+
+export default { size }
